a36a6e2117641071f04ea77c1352786e2ffd0a55
Mabende <belindamabende@gmail.com>
2024-06-18 22:47:49 +0200

    Initial Commit

---
+++ menu_R15/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  menu_R15
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/06/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ menu_R15/menu_R15App.swift
@@ -0,0 +1,17 @@
+//
+//  menu_R15App.swift
+//  menu_R15
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/06/18.
+//
+
+import SwiftUI
+
+@main
+struct menu_R15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
